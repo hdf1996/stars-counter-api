04afb0f1778639627e472576a7acfcdb35b13265
hdf1986 <hugo.farji+github@gmail.com>
2019-06-09 00:04:54 -0300

    Added avatar url

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const query = async (login, from, acc) => {
   const result = await graphql(`query ($login: String!, $from: String){
                                         user(login: $login) {
                                           login
-                                          repositories(first: 100, after: $from, isFork: false) {
+                                          avatarUrl
+                                          repositories(first: 10, after: $from, isFork: false) {
                                             totalCount
 
                                             edges {
@@ -35,14 +36,17 @@ const query = async (login, from, acc) => {
   if (result.user.repositories.totalCount > acc.length) {
     return query(login, acc[acc.length - 1][0], acc);
   } else {
-    return acc;
+    return [acc, result.user.avatarUrl];
   }
 }
 
 app.get('/stars/:login', async (req, res) => {
   console.log(`Requesting count for ${req.params.login}`)
   const result = await query(req.params.login, null, []);
-  res.send({total: result.reduce((a, b) => a + b[1], 0) })
+  res.send({
+    avatar: result[1],
+    total: result[0].reduce((a, b) => a + b[1], 0)
+  })
 })
 
 app.listen(port, () => console.log(`Stars count app listening on port ${port}!`))
